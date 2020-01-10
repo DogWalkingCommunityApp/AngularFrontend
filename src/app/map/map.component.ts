cdf27e9bc62ea54ff04fc0d16749d720e349668f
Dustin Gogoll <dustin.gogoll@googlemail.com>
2020-01-10 11:33:13 +0100

    Added marker via background-image

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
+import { MapCoordinates } from './map.interfaces';
 
 @Component({
   selector: 'app-map',
@@ -24,12 +25,14 @@ export class MapComponent implements AfterViewInit {
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((geoLocation: Position) => {
+        const coords: MapCoordinates = { lat: geoLocation.coords.latitude, lng: geoLocation.coords.longitude };
+
         const map = new this.HereMap.Map(
           document.getElementById('map'),
           defaultLayers.vector.normal.map,
           {
             zoom: 16,
-            center: { lat: geoLocation.coords.latitude, lng: geoLocation.coords.longitude },
+            center: coords,
             pixelRatio: window.devicePixelRatio || 1
           }
         );
@@ -46,14 +49,57 @@ export class MapComponent implements AfterViewInit {
         const behavior = new this.HereMap.mapevents.Behavior(new this.HereMap.mapevents.MapEvents(map));
         //const ui = this.HereMap.ui.UI.createDefault(map, defaultLayers);
     
-
+/*
         const icon = new this.HereMap.map.Icon('assets/icon/direction-marker.svg');
         const marker = new this.HereMap.map.Marker({ lat: geoLocation.coords.latitude, lng: geoLocation.coords.longitude }, { icon: icon });
         const group = new this.HereMap.map.Group();
+        console.log(icon)
+
         map.addObject(group);
-        group.addObject(marker);
+        group.addObject(marker);*/
+
+        this.addMarker(map, coords, 'assets/icon/direction-marker.svg', 'map-marker-user');
       })
     }
+  }
+
+  addMarker(map: any, coords: MapCoordinates, imagePath: string, className: string) {
+    const outerElement = document.createElement('div'),
+        innerElement = document.createElement('img');
+
+    innerElement.src = imagePath;
+    outerElement.classList.add(className);
+  
+  
+    //outerElement.appendChild(innerElement);
+  
+    const changeOpacity = (evt) => {
+      evt.target.style.opacity = 0.6;
+    };
+  
+    const changeOpacityToOne = (evt) => {
+      evt.target.style.opacity = 1;
+    };
+  
+    //create dom icon and add/remove opacity listeners
+    var domIcon = new this.HereMap.map.DomIcon(outerElement, {
+      // the function is called every time marker enters the viewport
+      onAttach: (clonedElement, domIcon, domMarker) => {
+        clonedElement.addEventListener('mouseover', changeOpacity);
+        clonedElement.addEventListener('mouseout', changeOpacityToOne);
+      },
+      // the function is called every time marker leaves the viewport
+      onDetach: (clonedElement, domIcon, domMarker) => {
+        clonedElement.removeEventListener('mouseover', changeOpacity);
+        clonedElement.removeEventListener('mouseout', changeOpacityToOne);
+      }
+    });
+  
+    // Marker for Chicago Bears home
+    var bearsMarker = new this.HereMap.map.DomMarker(coords, {
+      icon: domIcon
+    });
+    map.addObject(bearsMarker);
   }
 
 }
