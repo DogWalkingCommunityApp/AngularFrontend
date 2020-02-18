7be349bf9b2dd89b1a9a869d0fc0df81a9e1c3c4
Jojo <johaque@yahoo.de>
2020-02-18 14:33:34 +0100

    with 'origin/login-page'.

--- src/app/passwort-vergessen/passwort-vergessen.component.ts
+++ src/app/passwort-vergessen/passwort-vergessen.component.ts
@@ -1,14 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import config from '../environment.json';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-passwort-vergessen',
   templateUrl: './passwort-vergessen.component.html',
   styleUrls: ['./passwort-vergessen.component.scss'],
 })
-export class PasswortVergessenComponent implements OnInit {
+export class PasswortVergessenComponent {
+  private email: string;
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
 
-  ngOnInit() {}
+  async onSend() {
+    if(this.email.length > 5 && this.email.indexOf('@') !== -1 && this.email.indexOf('.') !== -1){
+      const alert = await this.alertController.create({
+        message: 'Die E-Mail wurde erfolgreich gesendet!',
+        buttons: ['OK']
+      });
+      await alert.present();
+    }
+
+
+
+    // const response = await fetch(config.serverBaseUrl + 'passwordForgotten', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(this.email)
+    // })
+    //
+    // const responseData = await response.json();
+    //
+    // console.log(responseData);
+  }
 
 }
