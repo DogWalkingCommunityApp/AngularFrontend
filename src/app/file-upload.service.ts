f56e6d60c6dd198edc0cffd4e2cdd2eaf2c95f3c
juwi <juliane.wippler@gmx.de>
2020-02-20 12:20:11 +0100

    POST url + build post object

--- src/app/file-upload.service.ts
+++ src/app/file-upload.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
+import {DataStoreService} from './data-store.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,16 @@ export class FileUploadService {
   private httpClient: any;
   private yourHeadersConfig: any;
 
-  constructor() { }
+  constructor(private dataStore: DataStoreService) { }
 
   postFile(fileToUpload: File): Observable<boolean> {
-    const endpoint = 'your-destination-url';
+    const endpoint = 'http://localhost:3000/profile/updateProfilePic';
     const formData: FormData = new FormData();
-    formData.append('fileKey', fileToUpload, fileToUpload.name);
+    formData.append('username', this.dataStore.userData.username, this.dataStore.userData.username);
+    formData.append('email', this.dataStore.userData.email, this.dataStore.userData.email);
+    formData.append('password', this.dataStore.userData.password, this.dataStore.userData.password);
+    formData.append('authId', this.dataStore.userData.auth, this.dataStore.userData.auth);
+    formData.append('profilePic', fileToUpload, fileToUpload.name);
     return this.httpClient
         .post(endpoint, formData, { headers: this.yourHeadersConfig })
         .map(() => true)
