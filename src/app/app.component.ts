412f5209e33abf14092dbdee8d5c9715e46e085d
Patrick Wehrheim <Patrick.Wehrheim@dbh.de>
2020-03-12 13:02:36 +0100

    #366830

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { PushNotificationService} from "./services/push-notification.service";
 
 @Component({
   selector: 'app-root',
@@ -16,8 +15,7 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-    pushService: PushNotificationService
+    private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
