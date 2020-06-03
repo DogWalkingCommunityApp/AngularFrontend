ea9ad2bfaf57f2b37d98f1e11de187f2c05d7370
martinpdh <42863666+martinpdh@users.noreply.github.com>
2020-06-03 12:37:01 +0200

    strangers-dog-index-card hinzugefügt
    bei dem fremden profil und dem eigenen werden nun dementsprechende fremde und eigene Hundekarten angezeigt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { PictureUploaderComponent } from './picture-uploader/picture-uploader.component';
 import { DogIndexCardComponent } from './dog-index-card/dog-index-card.component';
+import { StrangersDogIndexCardComponent } from './strangers-dog-index-card/strangers-dog-index-card.component';
 import { MessageBoxComponent } from './message-box/message-box.component';
 import { FriendListComponent } from './list/friend-list/friend-list.component';
 import { DogListComponent } from './list/dog-list/dog-list.component';
@@ -27,24 +28,24 @@ import { RoutesListComponent } from './list/routes-list/routes-list.component';
 import { BlockedListComponent } from './list/blocked-list/blocked-list.component';
 
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {PasswortVergessenComponent} from './passwort-vergessen/passwort-vergessen.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PasswortVergessenComponent } from './passwort-vergessen/passwort-vergessen.component';
 import { MustMatchDirective } from './must-match.directive';
-import {AddDogFormComponent} from './add-dog-form/add-dog-form.component';
+import { AddDogFormComponent } from './add-dog-form/add-dog-form.component';
 import { ValidateValueDirective } from './validate-value.directive';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ModalComponent } from './modal/modal.component';
 import { UploadProfilePictureComponent } from './upload-profile-picture/upload-profile-picture.component';
-import { PushNotificationService} from "./services/push-notification.service";
-import { environment} from "../environments/environment";
+import { PushNotificationService } from './services/push-notification.service';
+import { environment} from '../environments/environment';
 import { HttpClientModule } from '@angular/common/http';
 
 import { DataStoreService } from './services/data-store.service';
 import {StrangersProfileComponent} from './strangers-profile/strangers-profile.component';
-import {PushNotificationComponent} from "./push-notification/push-notification.component";
-import { WeatherComponent } from "./weather/weather.component";
+import {PushNotificationComponent} from './push-notification/push-notification.component';
+import { WeatherComponent } from './weather/weather.component';
 
 
 const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
@@ -62,6 +63,7 @@ const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
     RegistrationComponent,
     PictureUploaderComponent,
     DogIndexCardComponent,
+    StrangersDogIndexCardComponent,
     AddDogFormComponent,
     MustMatchDirective,
     MessageBoxComponent,
